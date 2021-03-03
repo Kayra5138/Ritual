d40efdf8815271f63f06921e7810719263364800
Enes Osmanoğlu <enesosmanoglu@gmail.com>
2021-03-03 13:43:15 +0300

    Player Movement

---
+++ RitualGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float moveSpeed= 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector2(xAxis, yAxis)* Time.fixedDeltaTime * moveSpeed);
+        //rb.AddRelativeForce(new Vector2(xAxis, yAxis)*Time.fixedDeltaTime*moveSpeed);
+    }
+}
